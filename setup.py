112ab5daa4d34bba56b07c5ae3ea69af7f1c4b6a
matham <moiein2000@gmail.com>
2020-05-27 16:23:12 -0400

    Plyer needs pyobjus on osx.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'ruamel.yaml', 'kivy', 'kivy_garden.filebrowser~=1.1.2', 'plyer',
-        'pywin32; sys_platform == "win32"'],
+        'pywin32; sys_platform == "win32"', 'pyobjus; sys_platform == "darwin"'],
     extras_require={
         'dev': ['pytest>=3.6', 'pytest-cov', 'flake8', 'sphinx-rtd-theme',
                 'coveralls'],
